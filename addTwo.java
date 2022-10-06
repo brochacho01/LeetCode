f1c66198783447f355ca4a208c6ac8f7b565a69d
Lachlan Imel <imell@cf164-07.cs.wwu.edu>
2022-10-06 09:59:15 -0700

    Solution for addTwo problem, beats 46.47%

--- addTwo.java
+++ addTwo.java
@@ -1,3 +1,5 @@
+package leetcode;
+
 public class addTwo {
     // https://leetcode.com/problems/add-two-numbers/
     public static void main(String[] args) {
@@ -12,8 +14,22 @@ public class addTwo {
         // l2.next = l22;
         // l22.next = l23;
 
-        ListNode l1 = new ListNode(0, null);
-        ListNode l2 = new ListNode(0, null);
+        // ListNode l1 = new ListNode(0, null);
+        // ListNode l2 = new ListNode(0, null);
+
+        // ListNode l1 = new ListNode(9, null);
+        // ListNode l12 = new ListNode(9, null);
+        // ListNode l13 = new ListNode(1, null);
+        // l1.next = l12;
+        // l12.next = l13;
+        // ListNode l2 = new ListNode(1, null);
+
+        ListNode l1 = new ListNode(3, null);
+        ListNode l12 = new ListNode(7, null);
+        ListNode l2 = new ListNode(9);
+        ListNode l22 = new ListNode(2);
+        l1.next = l12;
+        l2.next = l22;
         ListNode result = addTwoNumbers(l1, l2);
         System.out.print("");
     }
@@ -22,69 +38,78 @@ public class addTwo {
         ListNode result = new ListNode(0, null);
         ListNode trail = new ListNode(0, null);
         int resultint;
-        int carry;
-        if (l1.val + l2.val > 9) {
-            resultint = (l1.val + l2.val) - 10;
+        int carry = 0;
+        // Need to assign value to result as it is guaranteed to exist
+        if ((l1.val + l2.val) > 9) {
+            resultint = l1.val + l2.val - 10;
             carry = 1;
         } else {
             resultint = l1.val + l2.val;
-            carry = 0;
         }
         result.val = resultint;
+        l1 = l1.next;
+        l2 = l2.next;
         // Iterate over the lists when they have same degree of values
-        if ((l1.next != null) && (l2.next != null)) {
+        if ((l1 != null) || (l2 != null) || (carry == 1)) {
+            // If we have more than one node, attach result to trail
             result.next = trail;
-            while ((l1.next != null) && (l2.next != null)) {
-                l1 = l1.next;
-                l2 = l2.next;
-                if (l1.val + l2.val + carry > 9) {
-                    resultint = (l1.val + l2.val + carry) - 10;
+        }
+        // Loop over the parallel linked lists
+        while ((l1 != null) && (l2 != null)) {
+            if ((l1.val + l2.val + carry) > 9) {
+                resultint = (l1.val + l2.val + carry) - 10;
+                carry = 1;
+            } else {
+                resultint = l1.val + l2.val + carry;
+                carry = 0;
+            }
+            trail.val = resultint;
+            if ((l1.next != null) || (l2.next != null) || (carry == 1)) {
+                ListNode trail2 = new ListNode(0, null);
+                trail.next = trail2;
+                trail = trail.next;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (l1 != null) {
+            while (l1 != null) {
+                if (l1.val + carry > 9) {
+                    resultint = l1.val + carry - 10;
                     carry = 1;
                 } else {
-                    resultint = l1.val + l2.val + carry;
+                    resultint = l1.val + carry;
                     carry = 0;
                 }
                 trail.val = resultint;
-                if ((l1.next != null) && (l2.next != null)) {
+                if ((l1.next != null) || (carry == 1)) {
                     ListNode trail2 = new ListNode(0, null);
                     trail.next = trail2;
                     trail = trail.next;
                 }
-            }
-        }
-        if (l1.next != null) {
-            while (l1.next != null) {
                 l1 = l1.next;
-                if(l1.val + carry > 9){
-                    trail.val = 0;
-                    carry = 1;
-                } else {
-                    trail.val = resultint + carry;
-                    carry = 0;
-                }
-                if (l1.next != null) {
-                    ListNode trail2 = new ListNode(0, null);
-                    trail.next = trail2;
-                    trail = trail.next;
-                }
             }
         }
-        if (l2.next != null) {
-            while (l2.next != null) {
-                l2 = l2.next;
-                if(l2.val + carry > 9){
-                    trail.val = 0;
+        if (l2 != null) {
+            while (l2 != null) {
+                if (l2.val + carry > 9) {
+                    resultint = l2.val + carry - 10;
                     carry = 1;
                 } else {
-                    trail.val = resultint + carry;
+                    resultint = l2.val + carry;
                     carry = 0;
                 }
-                if (l2.next != null) {
+                trail.val = resultint;
+                if ((l2.next != null) || (carry == 1)) {
                     ListNode trail2 = new ListNode(0, null);
                     trail.next = trail2;
                     trail = trail.next;
                 }
+                l2 = l2.next;
             }
+        }
+        if (carry == 1) {
+            trail.val = 1;
         }
         return result;
     }
