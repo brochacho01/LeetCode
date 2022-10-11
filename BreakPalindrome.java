204fb932e701fb66336b26d8432f590f42c4c00b
Lachlan Imel <imell@cf405-24.cs.wwu.edu>
2022-10-11 11:31:33 -0700

    Solution that beats 100% for runtime and 72.2% for space

--- BreakPalindrome.java
+++ BreakPalindrome.java
@@ -1,9 +1,10 @@
 public class BreakPalindrome {
     // https://leetcode.com/problems/break-a-palindrome/
-    public static void main(String[] args){
-        String palindrome = "aa";
+    public static void main(String[] args) {
+        String palindrome = "aba";
         System.out.println(breakPalindrome(palindrome));
     }
+
     public static String breakPalindrome(String palindrome) {
         // Put string into array
         int strLen = palindrome.length();
@@ -11,29 +12,48 @@ public class BreakPalindrome {
             return "";
         }
         char[] palin = palindrome.toCharArray();
-        int j = 0;
-        int minChar = 'a';
         int mid = -1;
         int isSwap = 0;
-        if(strLen % 2 != 0) {
-            mid = (strLen / 2); 
+        if (strLen % 2 != 0) {
+            mid = strLen / 2;
         }
-        if(palin[0] != 'a') {
+        if (palin[0] != 'a') {
             palin[0] = 'a';
             isSwap = 1;
         }
-        while(isSwap == 0){
-            // TODO iterate over first string, check for minChar sub
-            // Iterate over entire string
-            for(int i = 1; i < strLen; i++){
-                if((palin[i] != ((char)(minChar + j))) && (i != mid)){
-                    palin[i] = ((char)(minChar + j));
+        // Iterate over first half of string
+        if (isSwap == 0) {
+            for (int i = 1; i < strLen / 2; i++) {
+                if ((palin[i] != 'a') && (i != mid)) {
+                    palin[i] = 'a';
                     isSwap = 1;
+                }
+                if (isSwap == 1) {
                     break;
                 }
             }
-            // TODO if no minchar sub, check second half of string to put in minChar + j
-            j++;
+        }
+        if (isSwap == 0) {
+            for (int i = strLen / 2; i < strLen - 1; i++) {
+                if ((palin[i] != 'a') && (i != mid)) {
+                    palin[i] = 'a';
+                    isSwap = 1;
+                }
+                if (isSwap == 1) {
+                    break;
+                }
+            }
+        }
+        if (isSwap == 0) {
+            for (int i = strLen - 1; i > 0; i--) {
+                if (palin[i] == 'a') {
+                    palin[i] = 'b';
+                    isSwap = 1;
+                }
+                if (isSwap == 1) {
+                    break;
+                }
+            }
         }
         String result = new String(palin);
         return result;
