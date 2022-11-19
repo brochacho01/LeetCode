4a27d938665fd8e435e5e52067359efebffe58d3
Lachlan Imel <lachlan@imelshire.com>
2022-11-19 00:36:31 -0800

    Dynamic solution to the jump game 2. Allegedly only beats 22%, perhaps a faster solution

---
+++ jumpGame2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # This is a dp problem
+        nLen = len(nums)
+        dp = [0]*nLen
+        # Want to iterate over all nums
+        for i in range(nLen):
+            ahead = nums[i]
+            # want to update every jump ahead
+            for j in range(i + 1, i + ahead + 1):
+                if j > nLen - 1:
+                    break
+                # Update a cell if it hasn't been set, or if current path is more optimal than previously calculate path
+                if dp[j] == 0 or dp[i] + 1 < dp[j]:
+                    dp[j] = dp[i] + 1
+                if j == nLen - 1:
+                    return dp[j]
+        return dp[nLen - 1]
