5725c648e7bec3fd1a8e5aaeb560431dcf12827f
Lachlan Imel <lachlan@imelshire.com>
2022-11-20 14:39:20 -0800

    Inefficient solution that only beats 15%

---
+++ anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Turn each string into a char array
+        # Sort the char array
+        # then use the char array as key in a dict with string as value
+        # if key already in, add string
+        # build up output with all the strings
+        sLen = len(strs)
+        dc = {}
+        for i in range(sLen):
+            curStr = strs[i]
+            curList = list(curStr)
+            List.sort(curList)
+            # turn List back into string of sorted characters to use as a key
+            curStr = str(curList)
+            if dc.get(curStr, -1) == -1:
+                dc.update({curStr : [strs[i]]})
+            else:
+                stList = dc.get(curStr)
+                stList.append(strs[i])
+                dc.update({curStr : stList})
+        result = []
+        for key in dc.keys():
+            result.append(dc.get(key))
+        return result
